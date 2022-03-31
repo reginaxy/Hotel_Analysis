8b3bea691184e46ab573c602e70ce892e5caac83
reginaxy <68020569+reginaxy@users.noreply.github.com>
2022-03-31 22:16:22 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -110,15 +110,22 @@ def main():
         lda_model = gensim.models.ldamodel.LdaModel.load('streamlit_template/LDAmallet_NOUNS')
           # Show Topics
         st.header("Topic Keywords")
+        
+        CHOICES = {0: "1", 1: "2", 2: "3", 3: "4", 4: "5", 5: "6", 6: "7", 7: "8", 8: "9", 9: "10", 10: "11", \
+                   11: "12", 12: "13", 13: "14", 14: "15", 15: "16", 16: "17", 18: "19", 19: "20"}
+        def format_func(option):
+            return CHOICES[option]
+        option = st.selectbox("Select option", options=list(CHOICES.keys()), format_func=format_func)
+        st.write(f"You selected option {option} called {format_func(option)}")
 
-        num = range(lda_model.num_topics) 
-        num_choice = st.write(num+1)
-        choice = st.multiselect("Pick Number of Topics to view", num_choice)
-        all_options = st.checkbox("Select all options")
-        if all_options:
-           choice = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+#         num = list(range(lda_model.num_topics) 
 
-        for t in choice:
+#         choice = st.multiselect("Pick Number of Topics to view", num)
+#         all_options = st.checkbox("Select all options")
+#         if all_options:
+#            choice = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+
+        for t in CHOICES:
           plt.figure()
           plt.imshow(WordCloud(background_color='white').fit_words(dict(lda_model.show_topic(t, 200))))
           plt.axis("off")
