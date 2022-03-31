8c94f19a5febdd310080df54e2ee5fb9740817e0
reginaxy <68020569+reginaxy@users.noreply.github.com>
2022-03-31 22:31:23 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -112,7 +112,7 @@ def main():
         st.header("Topic Keywords")
 
         num = range(lda_model.num_topics)
-        num_choice = {0:1, 1:2, 2:3, 3:4, 4:5, 5:6, 6:7, 7:8, 8:9, 9:10, 10:11, 11:12, 12:13, 13:14, 14:15, 15:16, 16:17, 17:18, 18:19, 19:20}
+        num_choice = num + 1
 
         choice = st.multiselect("Pick Number of Topics to view", num_choice)
         all_options = st.checkbox("Select all options")
