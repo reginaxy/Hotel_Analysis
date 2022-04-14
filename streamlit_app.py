c4f74aac6c84d06a3a3f8f950ac906f58ea4f301
reginaxy <68020569+reginaxy@users.noreply.github.com>
2022-04-15 01:00:53 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -90,7 +90,6 @@ def main():
       sent_model = pickle.load(open('streamlit_template/LR_SentAnalysis_IMPROVED.sav' , 'rb'))
       topic_model = pickle.load(open('streamlit_template/LR_Topic_Label.sav' , 'rb'))
       user_input = st.text_area("Enter a review to predict", "The check in process was straight forward, the room was very comfortable and clean. The staff were great, and the food was excellent too.")
-      st.button('PREDICT ▶️')  
     
         if clf_choice == 'Sentiment Classifier':
             st.header("Sentiment Classifier")  
