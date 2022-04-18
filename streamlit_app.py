8bdb5a4a98c5675a41320c51a28bfc7c6bd8a811
reginaxy <68020569+reginaxy@users.noreply.github.com>
2022-04-18 14:35:21 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -90,7 +90,7 @@ def main():
             
             class_labels=[topic_names[i] for i,prob in enumerate(y_pred) if prob > 0.5]
             label = st.subheader(class_labels)
-            st.markdown(f'<h1 style="color:#ff0000;text-align:center;font-size:24px;">{"label"}</h1>', unsafe_allow_html=True)
+            st.markdown(f'<h1 style="color:#ff0000;text-align:center;font-size:24px;">{label}</h1>', unsafe_allow_html=True)
             explainer = LimeTextExplainer(class_names=topic_names)
             exp = explainer.explain_instance(user_input, 
                                                 topic_model.predict_proba, 
